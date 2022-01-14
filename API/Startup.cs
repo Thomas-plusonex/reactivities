4cd7a9a72c1a307e1cc23af97ea9c07079c00134
Thomas <tadams@plusonex.com>
2022-01-14 13:16:01 +0200

    Section 4 - Adding basic CRUD for activities

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
+using Application.Activities;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -31,19 +35,7 @@ namespace API
     {
 
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-      });
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
-      });
-      services.AddCors(opt => {
-        opt.AddPolicy("NewPolicy", policy => {
-            policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
-          });
-      });
+      services.AddApplicationServices(_configuration);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
       setActivities(response.data);
     })
   }, []);
