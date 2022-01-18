15b6fc0d91fd0a99bf2a5d1a8fa08b56556f33b1
Thomas <tadams@plusonex.com>
2022-01-18 08:27:37 +0200

    Section 10 - Error Handling

---
+++ client-app/src/features/errors/ServerError.tsx
@@ -0,0 +1,32 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Container, Header, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+
+function ServerError() {
+  const {
+    commonStore
+  } = useStore();
+
+  const {
+    error,
+    setServerError
+  } = commonStore;
+
+
+  return (
+    <Container>
+      <Header as='h1' content='Server Error' />
+      <Header as='h5' color='red' content={error?.message} />
+      {
+        error?.details &&
+        <Segment>
+          <Header as='h4' content='Stack Trace' color="teal" />
+          <code style={{ marginTop: '10px' }}>{error?.details}</code>
+        </Segment>
+      }
+    </Container>
+  )
+}
+
+export default observer(ServerError);
